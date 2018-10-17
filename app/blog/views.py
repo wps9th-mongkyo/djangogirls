2288734b2897351449bcbefc1bd59665a27d3156
JungMongKyo <dreamong91@gmail.com>
2018-10-17 17:01:13 +0900

    views.py에 render()함수를 사용해서 표현

--- app/blog/views.py
+++ app/blog/views.py
@@ -1,9 +1,15 @@
-from django.http import HttpResponse
-from django.template import loader
+import random
+
+from django.shortcuts import render
 
 
 def post_list(request):
-    template = loader.get_template('blog/post_list.html')
-    context = {}
-    content = template.render(context, request)
-    return HttpResponse(content)
+    context = {
+        'pokemon': random.choice(['피카츄', '파이리', '꼬부기']),
+    }
+    # return render(
+    #     request=request,
+    #     template_name='blog/post_list.html',
+    #     context=context
+    # )
+    return render(request, 'blog/post_list.html', context)
