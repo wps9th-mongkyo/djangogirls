934f71b589a5eb5937389c0935b7b8874b36018c
JungMongKyo <dreamong91@gmail.com>
2018-10-17 13:37:05 +0900

    templates파일을 만들고 view와 연동하여 post_list.html파일을 출력

--- app/blog/views.py
+++ app/blog/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+import os
 
-# Create your views here.
+from django.http import HttpResponse, request
+from django.utils import timezone
+
+
+def post_list(request):
+    current_path = os.path.abspath(__file__)
+    blog_path = os.path.dirname(current_path)
+    app_path = os.path.dirname(blog_path)
+    post_list_path = os.path.join(app_path, 'templates', 'blog', 'post_list.html')
+
+    with open(post_list_path, 'rt') as f:
+        content = f.read()
+    return HttpResponse(content)
